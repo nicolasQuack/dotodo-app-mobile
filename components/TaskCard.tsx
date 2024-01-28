cc5e6167fc25177d25ec4dafd932deca42e7ecb1
nicolasQuack <nicolas.quack@icloud.com>
2024-01-28 03:21:55 -0300

    feat(/components): add handleComplete and delete functionallity to the TaskCard component;

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, View } from 'react-native'
 import React from 'react'
 import { Task } from '@/@types'
-import { Surface } from 'react-native-paper';
+import { IconButton, Surface, useTheme } from 'react-native-paper';
 import Typography from './Typography';
 import Separator from './Separator';
 
@@ -10,9 +10,11 @@ interface TaskCardProps extends Task {
   onDelete: () => void;
 }
 
-export default function TaskCard({ onComplete, onDelete, description,createdAt }: TaskCardProps) {
+export default function TaskCard({ onComplete, onDelete, description, createdAt, isCompleted }: TaskCardProps) {
 
   const formatedData = new Date(createdAt).toLocaleDateString()
+
+  const { colors } = useTheme()
 
 
   return (
@@ -20,7 +22,12 @@ export default function TaskCard({ onComplete, onDelete, description,createdAt }
       <Typography variant='titleLarge'>{description}</Typography>
       <Separator />
       <View style={styles.row}>
-    <Typography variant='titleMedium' color='outline'>{formatedData}</Typography>
+        <Typography variant='titleMedium' color='outline'>{formatedData}</Typography>
+        <View style={styles.row}>
+          <IconButton icon={'delete'} iconColor={colors.error} onPress={onDelete} />
+          <View style={[styles.separator,{backgroundColor:colors.outlineVariant}]}/>
+          <IconButton icon={isCompleted ? 'minus' : 'check'} iconColor={isCompleted ? colors.secondary : colors.primary} onPress={onComplete} />
+        </View>
       </View>
     </Surface>
   )
@@ -30,12 +37,15 @@ const styles = StyleSheet.create({
   container: {
     padding: 16,
     borderRadius: 15,
-    minHeight: 150,
     marginVertical: 8
   },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between'
+  },
+  separator:{
+    height: 40,
+    width:1
   }
 })
