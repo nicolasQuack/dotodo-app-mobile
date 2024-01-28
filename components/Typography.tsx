fb87e09dcd63c0f7073b8d43d5b39a5c8916b3de
nicolasQuack <nicolas.quack@icloud.com>
2024-01-28 03:01:24 -0300

    feat(/components): create new Typography component props;

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -1,14 +1,20 @@
 
 import React from 'react'
-import { Text, TextProps } from 'react-native-paper'
+import { Text, TextProps, useTheme } from 'react-native-paper'
 import { Text as BaseText } from 'react-native'
+import { MD3Colors } from 'react-native-paper/lib/typescript/types'
 
 interface Props extends TextProps<BaseText> {
-    fontFamily?: 'RobotoRegular' | 'Pacifico' | 'SpaceMono'
+    fontFamily?: 'RobotoRegular' | 'Pacifico' | 'SpaceMono',
+    color?: keyof MD3Colors
 }
 
-export default function Typography({ fontFamily = 'RobotoRegular', style, children, ...rest }: Props) {
+export default function Typography({ fontFamily = 'RobotoRegular', style, color = 'onBackground', children, ...rest }: Props) {
+
+
+    const { colors } = useTheme()
+
     return (
-        <Text style={[style, { fontFamily }]} {...rest}>{children}</Text>
+        <Text style={[{ fontFamily, color: colors[color] as string }, style,]} {...rest}>{children}</Text>
     )
 }
